7578a9dc16488dac33147352c5f17fc958036280
iamna2000 <50236920+iamna2000@users.noreply.github.com>
2021-09-16 06:30:56 +0700

    db

--- shipapp/ship/models.py
+++ shipapp/ship/models.py
@@ -46,3 +46,13 @@ class Rating(models.Model):
     rating = models.DecimalField(decimal_places=0, max_digits=5)
     post = models.ForeignKey(Post, on_delete=models.SET_NULL, null=True)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Post_tags(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.SET_NULL, null=True)
+    tag = models.ForeignKey(Tag, on_delete=models.SET_NULL, null=True)
